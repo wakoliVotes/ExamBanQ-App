a85c70842eca007470fd5c959671c6a5cfd8aba3
wakolivotes <votesdw@yahoo.com>
2022-12-15 11:38:34 +0300

    Academic Calendar

---
+++ ExamBankCompose/app/src/main/java/com/example/exambank/modeldatautils/AcademicCalendar.kt
@@ -0,0 +1,7 @@
+package com.example.exambank.modeldatautils
+
+data class AcademicCalendar(
+    val folderImg: Int,
+    val academicPeriodYear: String,
+    val academicMonths: String
+)
---
+++ ExamBankCompose/app/src/main/java/com/example/exambank/modeldatautils/AcademicCalendarData.kt
@@ -0,0 +1,98 @@
+package com.example.exambank.modeldatautils
+
+import com.example.exambank.R
+
+object AcademicCalendarData {
+    val academicList = listOf(
+        AcademicCalendar(
+            folderImg = R.drawable.login_img,
+            academicPeriodYear = "2011",
+            academicMonths = "May-August"
+        ),
+        AcademicCalendar(
+            folderImg = R.drawable.login_img,
+            academicPeriodYear = "2012",
+            academicMonths = "Jan-April"
+        ),
+        AcademicCalendar(
+            folderImg = R.drawable.login_img,
+            academicPeriodYear = "2012",
+            academicMonths = "May-August"
+        ),
+        AcademicCalendar(
+            folderImg = R.drawable.login_img,
+            academicPeriodYear = "2012",
+            academicMonths = "Sept-Dec"
+        ),
+        AcademicCalendar(
+            folderImg = R.drawable.login_img,
+            academicPeriodYear = "2013",
+            academicMonths = "May-August"
+        ),
+        AcademicCalendar(
+            folderImg = R.drawable.login_img,
+            academicPeriodYear = "2014",
+            academicMonths = "Jan-April"
+        ),
+        AcademicCalendar(
+            folderImg = R.drawable.login_img,
+            academicPeriodYear = "2014",
+            academicMonths = "May-August"
+        ),
+        AcademicCalendar(
+            folderImg = R.drawable.login_img,
+            academicPeriodYear = "2014",
+            academicMonths = "Sept-Dec"
+        ),
+        AcademicCalendar(
+            folderImg = R.drawable.login_img,
+            academicPeriodYear = "2015",
+            academicMonths = "Jan-April"
+        ),
+        AcademicCalendar(
+            folderImg = R.drawable.login_img,
+            academicPeriodYear = "2016",
+            academicMonths = "Sept-Dec"
+        ),
+        AcademicCalendar(
+            folderImg = R.drawable.login_img,
+            academicPeriodYear = "2017",
+            academicMonths = "Jan-April"
+        ),
+        AcademicCalendar(
+            folderImg = R.drawable.login_img,
+            academicPeriodYear = "2019",
+            academicMonths = "May-August"
+        ),
+        AcademicCalendar(
+            folderImg = R.drawable.login_img,
+            academicPeriodYear = "2019",
+            academicMonths = "May-August"
+        ),
+        AcademicCalendar(
+            folderImg = R.drawable.login_img,
+            academicPeriodYear = "2019",
+            academicMonths = "May-August"
+        ),
+        AcademicCalendar(
+            folderImg = R.drawable.login_img,
+            academicPeriodYear = "2019",
+            academicMonths = "May-August"
+        ),
+        AcademicCalendar(
+            folderImg = R.drawable.login_img,
+            academicPeriodYear = "2019",
+            academicMonths = "May-August"
+        ),
+        AcademicCalendar(
+            folderImg = R.drawable.login_img,
+            academicPeriodYear = "2019",
+            academicMonths = "May-August"
+        ),
+        AcademicCalendar(
+            folderImg = R.drawable.login_img,
+            academicPeriodYear = "2019",
+            academicMonths = "May-August"
+        ),
+    )
+}
