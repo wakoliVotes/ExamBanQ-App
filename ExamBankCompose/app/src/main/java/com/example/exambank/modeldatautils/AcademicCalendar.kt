d659c0f5fb11f43b309010345baefd52283eb188
wakolivotes <votesdw@yahoo.com>
2022-12-15 11:56:50 +0300

    Academic Calendar

--- ExamBankCompose/app/src/main/java/com/example/exambank/modeldatautils/AcademicCalendar.kt
+++ ExamBankCompose/app/src/main/java/com/example/exambank/modeldatautils/AcademicCalendar.kt
@@ -3,5 +3,6 @@ package com.example.exambank.modeldatautils
 data class AcademicCalendar(
     val folderImg: Int,
     val academicPeriodYear: String,
-    val academicMonths: String
+    val academicMonths: String,
+    val itsStatus: String
 )
--- ExamBankCompose/app/src/main/java/com/example/exambank/modeldatautils/AcademicCalendarData.kt
+++ ExamBankCompose/app/src/main/java/com/example/exambank/modeldatautils/AcademicCalendarData.kt
@@ -7,92 +7,110 @@ object AcademicCalendarData {
         AcademicCalendar(
             folderImg = R.drawable.folder_round,
             academicPeriodYear = "2011",
-            academicMonths = "May-August"
+            academicMonths = "May-August",
+            itsStatus = "Coming Soon"
         ),
         AcademicCalendar(
             folderImg = R.drawable.folder_round,
             academicPeriodYear = "2012",
-            academicMonths = "Jan-April"
+            academicMonths = "Jan-April",
+            itsStatus = "Coming Soon"
         ),
         AcademicCalendar(
             folderImg = R.drawable.folder_round,
             academicPeriodYear = "2012",
-            academicMonths = "May-August"
+            academicMonths = "May-August",
+            itsStatus = "Coming Soon"
         ),
         AcademicCalendar(
             folderImg = R.drawable.folder_round,
             academicPeriodYear = "2012",
-            academicMonths = "Sept-Dec"
+            academicMonths = "Sept-Dec",
+            itsStatus = "Coming Soon"
         ),
         AcademicCalendar(
             folderImg = R.drawable.folder_round,
             academicPeriodYear = "2013",
-            academicMonths = "May-August"
+            academicMonths = "May-August",
+            itsStatus = "Coming Soon"
         ),
         AcademicCalendar(
             folderImg = R.drawable.folder_round,
             academicPeriodYear = "2014",
-            academicMonths = "Jan-April"
+            academicMonths = "Jan-April",
+            itsStatus = "Coming Soon"
         ),
         AcademicCalendar(
             folderImg = R.drawable.folder_round,
             academicPeriodYear = "2014",
-            academicMonths = "May-August"
+            academicMonths = "May-August",
+            itsStatus = "Coming Soon"
         ),
         AcademicCalendar(
             folderImg = R.drawable.folder_round,
             academicPeriodYear = "2014",
-            academicMonths = "Sept-Dec"
+            academicMonths = "Sept-Dec",
+            itsStatus = "Coming Soon"
         ),
         AcademicCalendar(
             folderImg = R.drawable.folder_round,
             academicPeriodYear = "2015",
-            academicMonths = "Jan-April"
+            academicMonths = "Jan-April",
+            itsStatus = "Coming Soon"
         ),
         AcademicCalendar(
             folderImg = R.drawable.folder_round,
             academicPeriodYear = "2016",
-            academicMonths = "Sept-Dec"
+            academicMonths = "Sept-Dec",
+            itsStatus = "Coming Soon"
         ),
         AcademicCalendar(
             folderImg = R.drawable.folder_round,
             academicPeriodYear = "2017",
-            academicMonths = "Jan-April"
-        ),
-        AcademicCalendar(
-            folderImg = R.drawable.login_img,
-            academicPeriodYear = "2017",
-            academicMonths = "May-August"
+            academicMonths = "Jan-April",
+            itsStatus = "Coming Soon"
         ),
         AcademicCalendar(
             folderImg = R.drawable.folder_round,
             academicPeriodYear = "2017",
-            academicMonths = "Sept-Dec"
+            academicMonths = "May-August",
+            itsStatus = "Coming Soon"
+        ),
+        AcademicCalendar(
+            folderImg = R.drawable.folder_round,
+            academicPeriodYear = "2017",
+            academicMonths = "Sept-Dec",
+            itsStatus = "Coming Soon"
         ),
         AcademicCalendar(
             folderImg = R.drawable.folder_round,
             academicPeriodYear = "2018",
-            academicMonths = "Jan-April"
+            academicMonths = "Jan-April",
+            itsStatus = "Coming Soon"
         ),
         AcademicCalendar(
             folderImg = R.drawable.folder_round,
             academicPeriodYear = "2018",
-            academicMonths = "May-August"
+            academicMonths = "May-August",
+            itsStatus = "Coming Soon"
         ),
         AcademicCalendar(
             folderImg = R.drawable.folder_round,
             academicPeriodYear = "2018",
-            academicMonths = "sEPT-Dec"
+            academicMonths = "sEPT-Dec",
+            itsStatus = "Coming Soon"
         ),
         AcademicCalendar(
             folderImg = R.drawable.folder_round,
             academicPeriodYear = "2019",
-            academicMonths = "Jan-April"
+            academicMonths = "Jan-April",
+            itsStatus = "Coming Soon"
         ),
         AcademicCalendar(
             folderImg = R.drawable.folder_round,
             academicPeriodYear = "2019",
-            academicMonths = "May-August"
+            academicMonths = "May-August",
+            itsStatus = "Coming Soon"
         )
     )
 }
