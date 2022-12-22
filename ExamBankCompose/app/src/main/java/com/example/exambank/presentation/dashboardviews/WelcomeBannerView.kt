d5a9c7a66012282b0558e7ca0ee94bb1e431a2f9
wakolivotes <votesdw@yahoo.com>
2022-12-22 14:23:51 +0300

    Home Banner

--- ExamBankCompose/app/src/main/java/com/example/exambank/presentation/dashboardviews/WelcomeBannerView.kt
+++ ExamBankCompose/app/src/main/java/com/example/exambank/presentation/dashboardviews/WelcomeBannerView.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -137,13 +138,16 @@ fun CreateWelcomeBanner(
 
                 {
                     Text(
-                        text = "Third",
+                        text = "Access Revision Materials",
                         style = TextStyle(
                             fontFamily = FontFamily.Monospace,
                             fontWeight = FontWeight.SemiBold,
                             color = Color.White
                         ),
-                        modifier = Modifier.align(Alignment.Center)
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                            .padding(10.dp),
+                        textAlign = TextAlign.Center
                     )
                 }
                 Box(
@@ -156,13 +160,16 @@ fun CreateWelcomeBanner(
                 )
                 {
                     Text(
-                        text = "Fourth",
+                        text = "Propel your learning journey with ease",
                         style = TextStyle(
                             fontFamily = FontFamily.Monospace,
                             fontWeight = FontWeight.SemiBold,
                             color = Color.White
                         ),
-                        modifier = Modifier.align(Alignment.Center)
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                            .padding(10.dp),
+                        textAlign = TextAlign.Center
                     )
                 }
             }
