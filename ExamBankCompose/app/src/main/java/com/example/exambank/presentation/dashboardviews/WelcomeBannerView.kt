3edb29a5bcee9a2028c4fb5f20e170ad6c4ea0cc
wakolivotes <votesdw@yahoo.com>
2022-12-23 22:42:31 +0300

    Welcome Banner changes

--- ExamBankCompose/app/src/main/java/com/example/exambank/presentation/dashboardviews/WelcomeBannerView.kt
+++ ExamBankCompose/app/src/main/java/com/example/exambank/presentation/dashboardviews/WelcomeBannerView.kt
@@ -19,9 +19,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
